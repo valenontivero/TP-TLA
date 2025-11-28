1022b442e0327a89e3a5b11a49b2a1cc211441ce
maurovella <mvella@itba.edu.ar>
2025-11-28 19:44:27 -0300

    feat: implement output conversion module for audio formats AFTER CODE GENERATION PHASE

--- src/main/c/backend/output-conversion/OutputConversion.h
+++ src/main/c/backend/output-conversion/OutputConversion.h
@@ -7,11 +7,6 @@
 /** Initialize module's internal state. */
 ModuleDestructor initializeOutputConversionModule();
 
-/**
- * Executes the output conversion stage, transforming the generated MIDI file
- * into one or more audio formats, depending on the compiler state and
- * environment configuration.
- */
 void executeOutputConversion(const CompilerState * compilerState);
 
 #endif
